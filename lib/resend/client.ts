746c09d1ffa7a8424f9911f538de0109f7db98e8
Ashutosh Swamy <ashutoshswamy397@gmail.com>
2026-07-12 13:51:11 +0530

    updated the email functionality 🚀

--- lib/resend/client.ts
+++ lib/resend/client.ts
@@ -1,18 +1,18 @@
 import { Resend } from "resend";
 
-const realResend = new Resend(process.env.RESEND_API_KEY);
-
 // ponytail: email sending disabled app-wide; flip to true (or remove this guard) to re-enable
 const EMAIL_ENABLED = false;
 
+type SendArgs = Parameters<Resend["emails"]["send"]>;
+
 export const resend = {
   emails: {
-    send: async (...args: Parameters<typeof realResend.emails.send>) => {
+    send: async (...args: SendArgs) => {
       if (!EMAIL_ENABLED) {
         console.log("[resend] email sending disabled, skipped:", args[0]?.subject);
         return { data: null, error: null };
       }
-      return realResend.emails.send(...args);
+      return new Resend(process.env.RESEND_API_KEY).emails.send(...args);
     },
   },
 };
